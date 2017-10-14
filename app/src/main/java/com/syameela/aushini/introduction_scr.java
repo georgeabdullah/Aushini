67d9418d88be0cace7032692a5d331ba5e2b9817
Syameela iMac <syameela@Syameelas-iMac.local>
2017-10-15 01:43:37 +0700

    4. Successfully added intro function and enable slider menu to see more features before users start to register themself. RESULT = SUCCESS

--- app/src/main/java/com/syameela/aushini/introduction_scr.java
+++ app/src/main/java/com/syameela/aushini/introduction_scr.java
@@ -1,13 +1,29 @@
 package com.syameela.aushini;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class introduction_scr extends AppCompatActivity {
+
+    Button buka_halaman_fitur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction_scr);
+
+        buka_halaman_fitur = (Button) findViewById(R.id.btn_next_welcome);
+
+        buka_halaman_fitur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(introduction_scr.this, welcome_scr.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
