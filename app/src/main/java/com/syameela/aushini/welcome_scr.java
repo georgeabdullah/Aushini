75e53013226186541ec066e402c8a905de3d042e
Syameela iMac <syameela@Syameelas-iMac.local>
2017-10-15 00:33:37 +0700

    1. First reconstruction after getting several errors after hibernation project from windows

---
+++ app/src/main/java/com/syameela/aushini/welcome_scr.java
@@ -0,0 +1,13 @@
+package com.syameela.aushini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome_scr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_scr);
+    }
+}
